f9feac477cf150ff95b904b917677e6a9746f9ff
syxtest01@gmail.com <xing_*666888>
2017-05-05 17:11:54 +0800

    添加IdentityServerWithAspNetIdentity验证方式

---
+++ 6_AspNetIdentity/AspNetIdentity.Api/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetIdentity.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "AspNetIdentity.Api";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5009")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
